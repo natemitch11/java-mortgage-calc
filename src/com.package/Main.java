8eb5b37336bdbf561dd4359a08e4bb1f333f6e4c
natemitch11 <nate.mitch11@gmail.com>
2021-09-12 14:06:42 -0600

    Mortgage Calculator with data validation loops in Java

---
+++ src/com.package/Main.java
@@ -0,0 +1,52 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        int principal = 0;
+        float annualInterest = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+        byte years = 0;
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Principal (between 1K and 1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Please enter a value between 1K and 1M");
+        }
+
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Please enter a value between 0 and 30");
+        }
+
+        while (true) {
+            System.out.print("Period (Years): ");
+            years = scanner.nextByte();
+            if (years > 0 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Please enter a value between 0 and 30");
+        }
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+}
